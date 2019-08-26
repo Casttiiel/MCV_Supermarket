6ceb9fb129f14863e9a600d42b0e8c30c62d3a8c
victor <vnovo@gmail.com>
2019-08-26 14:53:51 +0200

    +cosas UI

---
+++ source/modules/game/module_game_over.cpp
@@ -0,0 +1,34 @@
+#include "mcv_platform.h"
+#include "module_game_over.h"
+#include "entity/entity.h"
+#include "engine.h"
+#include "input/input.h"
+#include "components/common/comp_transform.h"
+#include "ui/controllers/ui_menu_controller.h"
+#include "ui/module_ui.h"
+#include "ui/widgets/ui_button.h"
+#include "module_game_manager.h"
+
+bool CModuleGameOver::start()
+{
+	UI::CModuleUI& ui = Engine.getUI();
+	//CEngine::get().getRender().setBackgroundColor(0.f, 0.f, 1.f, 1.f);
+	//CEngine::get().getRender().
+	return true;
+
+
+
+}
+
+void CModuleGameOver::update(float delta)
+{
+ 
+  
+}
+
+void CModuleGameOver::stop()
+{
+	
+}
+
+
