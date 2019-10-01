850d1d75416ed1f06a238fcb75c62931b029e55e
victor <vnovo@gmail.com>
2019-10-01 20:20:52 +0200

    Trampa de fuego de carnes

--- source/components/objects/comp_enemies_in_butcher.cpp
+++ source/components/objects/comp_enemies_in_butcher.cpp
@@ -2,6 +2,7 @@
 #include "comp_enemies_in_butcher.h"
 #include "components/controllers/character/comp_character_controller.h"
 #include "modules/game/module_game_controller.h"
+#include "components/objects/comp_platform_fire_trap.h"
 #include "engine.h"
 
 DECL_OBJ_MANAGER("enemies_in_butcher", TCompEnemiesInButcher);
@@ -22,8 +23,13 @@ void TCompEnemiesInButcher::load(const json& j, TEntityParseContext& ctx){
 
 
 void TCompEnemiesInButcher::update(float dt) {
+	
 	if (count >= 3) {
-		Scripting.execActionDelayed("activeElevatorInitCarniceria_player()", 0.0);
+		CHandle handle = GameController.entityByName("Platform Fire Particles");
+		CEntity* entity = (CEntity*)handle;
+		TCompFirePlatformTrap* firePlatformTrap = entity->get<TCompFirePlatformTrap>();
+		firePlatformTrap->flagActivate = false;
+		Scripting.execActionDelayed("activeElevatorInitCarniceria_player()", 1.6);
 		//desactivar llamas de la plataforma cuando este listo
 		GameController.destroyCHandleByName("enemies_in_butcher");
 		
