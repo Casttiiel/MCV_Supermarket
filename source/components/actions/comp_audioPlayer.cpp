91635b317b06cb362525d458d6f4017c5511d27d
Javier <10100300+Korn0zz@users.noreply.github.com>
2019-10-05 18:37:46 +0200

    Skewer platform audio implemented

--- source/components/actions/comp_audioPlayer.cpp
+++ source/components/actions/comp_audioPlayer.cpp
@@ -12,16 +12,25 @@ void TCompAudioPlayer::debugInMenu() {
 
 void TCompAudioPlayer::load(const json& j, TEntityParseContext& ctx) {
     _audioEventString = j.value("_audioEventString", _audioEventString);
+    _audioEventStringSlowVersion = j.value("_audioEventStringSlowVersion", _audioEventStringSlowVersion);
     _autoPlay = j.value("_autoPlay", _autoPlay);
     
     assert(_audioEventString != "");
     _audioEvent = EngineAudio.playEvent(_audioEventString);
     if (_audioEvent.is3D()) {
         TCompTransform* c_trans = get<TCompTransform>();
-        _audioEvent.set3DAttributes(*c_trans);
+        if(c_trans)
+            _audioEvent.set3DAttributes(*c_trans);
     }
     if(!_autoPlay){
         _audioEvent.stop();
+    }
+    if (_audioEventStringSlowVersion != "") {
+        TCompTransform* c_trans = get<TCompTransform>();
+        _audioEventSlow = EngineAudio.playEvent(_audioEventStringSlowVersion);
+        _audioEventSlow.setPaused(true);
+        if (c_trans)
+            _audioEventSlow.set3DAttributes(*c_trans);
     }
 }
 
@@ -43,6 +52,24 @@ AudioEvent TCompAudioPlayer::getEvent() {
 void TCompAudioPlayer::update(float dt) {
     if (_audioEvent.is3D()) {
         TCompTransform* c_trans = get<TCompTransform>();
-        _audioEvent.set3DAttributes(*c_trans);
+        if (c_trans)
+            _audioEvent.set3DAttributes(*c_trans);
+    }
+
+    if (_audioEventStringSlowVersion != "") {
+        if (GameController.getTimeScale() < 1.0f && _audioEventSlow.getPaused()) {
+            _audioEventSlow.setPaused(false);
+            _audioEvent.setPaused(true);
+        }
+        else if (GameController.getTimeScale() == 1.0f && _audioEvent.getPaused()) {
+            _audioEvent.setPaused(false);
+            _audioEventSlow.setPaused(true);
+        }
+
+        if (_audioEventSlow.is3D()) {
+            TCompTransform* c_trans = get<TCompTransform>();
+            if (c_trans)
+                _audioEventSlow.set3DAttributes(*c_trans);
+        }
     }
 }
