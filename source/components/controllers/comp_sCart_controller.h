dead2eb079a2934fa7201cbbecedba166e28cab4
Dani <casttiiel@gmail.com>
2019-10-07 20:17:19 +0200

    Chrom aberr mas disimulada, mejor impulso del carrito para la rampa

--- source/components/controllers/comp_sCart_controller.h
+++ source/components/controllers/comp_sCart_controller.h
@@ -45,12 +45,12 @@ private:
 	float speed = 6.0f;
 	float rowDelay = 1.0f;
 	float rowTimer = 0.f;
-	float rowImpulse = 12.f;
+	float rowImpulse = 15.f;
   float inclinationRow = 8.f;
 	float rowImpulseLeft = 0.f;
 	float rowImpulseLossRatio = 5.0f;
 	float rotation_speed = 1.3f;
-  float maxImpulse = 30.0f;
+  float maxImpulse = 40.0f;
 
 	float rumble_time = 0.0f;
 
